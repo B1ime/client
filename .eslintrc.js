92641e6a02b39980d4ba1f840d0966dd257422d9
Byeong Gyu Choi <bisschoi9541@gmail.com>
2018-10-23 03:49:38 +0900

    Dependency 업데이트

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,13 @@ module.exports = {
   extends: 'airbnb',
   parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 7
+    ecmaVersion: 7,
   },
   rules: {
     'arrow-parens': 'off',
     'no-console': 'off',
     'no-undef': 'off',
-    'react/jsx-filename-extension': 'off'
-  }
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+  },
 };
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # B1ime-client
+[![style: styled-components](https://img.shields.io/badge/style-%F0%9F%92%85%20styled--components-orange.svg?colorB=daa357&colorA=db748e)](https://github.com/styled-components/styled-components)
+
 
 ## Setup
 ```text
